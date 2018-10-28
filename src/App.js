09ea07338620912da6ad55b0c4fc295941219d7f
Youssef <ynakles@gmail.com>
2018-10-28 02:42:35 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React, { Component } from "react";
 import {
-	Form,
-	Input,
 	Divider,
-	Button
 } from "semantic-ui-react";
-import InfiniteLoop from './InfiniteLoop';
+import InfiniteLoop from 'infinite-loop-animation';
 
 const urls = [
 	"https://image.flaticon.com/icons/svg/413/413850.svg",
@@ -34,7 +31,6 @@ class App extends Component {
 		super(props);
 		this.state = {
 			color: '#fcfcfc',
-			anim:true,
 			form:false
 		};
 	}
@@ -62,55 +58,65 @@ class App extends Component {
 						<h1 style={{ color: this.state.color}}>
 							InfiniteLoop.
 						</h1>
+
+						<Divider hidden />
 						
 						<h3 style={{ color: this.state.color }}>
 						Easily make and manage React animation loops
 						</h3>
 						
-
-
 						<h6 style={{ color: '#b3cce6' }}>
-						Dynamic looping of React animations.
-						Define an animaton with urls, specify a duration, and the animation will repeat for that infinite iterations. 
+						Dynamic looping of image animations in React.
+						Define an animaton with urls or paths, specify a duration, and the animation will repeat for that infinite iterations. 
 						You can not change the number of iterations programmatically.
 						</h6>
+
 						<Divider hidden />
 
-						<InfiniteLoop data={urls} background={bg} duration={3000} size={75} />
+						<InfiniteLoop data={urls} background={bg} duration={800} size={65} />
 
-						<h3 style={{ color: this.state.color }}>
+						<Divider hidden />
+
+						<div style={{ color: this.state.color }}>
+						<h3>
 						Installation
 						</h3>
-						<img src="https://i.imgur.com/ln0MRWZ.png" alt="" style={{maxHeight: 100}} />
-				
-						<h3 style={{ color: this.state.color}}>
+
+						<pre><code style={{ color: 'rgb(179, 204, 230)' }}>
+						npm i infinite-loop-animation
+						</code></pre>
+
+						<h3 >
 						Sample usage
 						</h3>
-						<h6 style={{ color: '#b3cce6' }}>
-						Initial style for animated element, used for ReactMotion's defaultStyle and as returning animation.
-						Currently is being developed and tested with the latest stable Node 7 on OSX. 
-						To run example covering all ReactMotionLoop features, use npm start dev, which will compile src/example/Example.js
-						</h6>
+						</div>
+
+						<Divider hidden />	
+
+						<img src="https://i.imgur.com/UOCEBoT.png" alt="" style={{maxHeight: 500}} />
+
+						<Divider hidden />
+
+						  <h3 style={{ color: this.state.color }}>
+							Properties
+							</h3>
+
+						<div style={{ color: 'rgb(179, 204, 230)' }}>
+							<b>data</b>
+							<p>An array of images</p>
+							<b>background</b>
+							<p>Background image path or url</p>
+							<b>duration</b>
+							<p>Time between each frame in milliseconds</p>
+							<b>size</b>
+							<p>Recommended values between 25 and 250</p>
+						</div>
+
 
 					</div>
 				) : null}
 
-				<Divider hidden />
-
-				{this.state.form ? (
-					<div className="updater" onClick={this.svg}>
-						<svg
-							className="svgUpdate"
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 24 24"
-						><path d="M24 9h-9v-9h-6v9h-9v6h9v9h6v-9h9z" />
-						</svg>
-					</div>
-				) : null}
-
-
-
-				
+				<Divider hidden />		
 
 			</div>
 		);
